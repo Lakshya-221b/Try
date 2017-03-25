2b96494ca1325c1c69b0eb1115ba7d18cf3d9907
Lakshya-221b <iit2016009@iiita.ac.in>
2017-03-25 12:51:17 +0530

    aa

---
+++ app/src/main/java/com/example/au_009tx/atry/Config.java
@@ -0,0 +1,10 @@
+package com.example.au_009tx.atry;
+
+/**
+ * Created by AU-009TX on 23-03-2017.
+ */
+
+public class Config {
+    public static final String EMAIL ="dc";
+    public static final String PASSWORD ="dfd";
+}
